1b978b4418a633984cf4a506e299622b94fbb900
Tetsuro Ando <teando@c2r7s10.42tokyo.jp>
2024-12-16 18:06:39 +0900

    add lexer header and builtin rapper

--- src/7_builtin/echo.c
+++ src/7_builtin/echo.c
@@ -1,9 +1,31 @@
-#include "ft_env.h"
-#include "minishell_info.h"
-#include "ft_builtin.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/16 15:48:32 by teando            #+#    #+#             */
+/*   Updated: 2024/12/16 18:04:36 by teando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void builtin_echo(char **args)
+#include "minishell.h"
+
+int	exec_echo(char **argv)
 {
-    if (ft_strncmp(args[0], ")
+	int	i;
 
+	// argv[0]="echo", argv[1]="arg1", argv[2]="arg2", ...
+	// 基本的には改行付きで残りを出力。-nオプションなどは後で実装可能。
+	i = 1;
+	while (argv[i])
+	{
+		printf("%s", argv[i]);
+		if (argv[i + 1])
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
