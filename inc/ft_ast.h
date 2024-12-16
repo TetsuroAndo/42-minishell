8d8b149595616d7a3dc858408aa962ae24e09736
QWERTOP18 <mz18yui@gmail.com>
2024-12-16 15:55:36 +0900

    btmap new

---
+++ inc/ft_ast.h
@@ -0,0 +1,26 @@
+typedef enum e_token_type
+{
+    TOKEN_WORD,
+    TOKEN_PIPE,//|
+    TOKEN_REDIR_IN,//<
+    TOKEN_REDIR_OUT,//>
+    TOKEN_REDIR_APPEND,//>>
+    TOKEN_REDIR_HEREDOC,//<<
+    TOKEN_ENV_VAR,//$
+}    t_token_type;
+
+typedef struct s_token
+{
+    t_token_type        type;
+    char                *value;
+    struct s_token        *next;
+}    t_token;
+
+typedef struct s_ast_node
+{
+    t_token_type        type;
+   // int                    file_type;
+    char                **args;
+    struct s_ast_node    *left;
+    struct s_ast_node    *right;
+}    t_ast_node;
---
+++ inc/ft_env.h
@@ -0,0 +1,9 @@
+#ifndef FT_ENV_H
+#define FT_ENV_H
+
+#include <stdlib.h>
+#include "ft_btmap.h"
+
+
+
+#endif
