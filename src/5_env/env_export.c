0c42101fcf948e353e0f4c21aa94ea7fe5f4e71c
Yui Mizukami <ymizukam@c4r1s10.42tokyo.jp>
2024-12-18 21:11:50 +0900

    env

--- src/5_env/env_export.c
+++ src/5_env/env_export.c
@@ -6,7 +6,7 @@
 /*   By: ymizukam <ymizukam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 19:48:52 by ymizukam          #+#    #+#             */
-/*   Updated: 2024/12/18 20:19:47 by ymizukam         ###   ########.fr       */
+/*   Updated: 2024/12/18 20:44:44 by ymizukam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,17 @@ t_status	env_export(t_list *env, char *ent)
 		return (E_ENV_KEY);
 	if (!ent_cpy)
 		return (E_ALLOCATE);
-	deli = ft_strcspn(ent, "=");
+	deli = ft_strspn(ent, "=");
 	ft_strncpy(uniq_key, ent, deli + 1);
 	lst = ft_list_find(env, uniq_key, __cmp);
 	if (lst) //すでに存在する場合上書き
 	{
+		printf("key already exist%s\n", lst->data);
+		free(lst->data);
 		lst->data = ent_cpy;
 		return (E_NONE);
 	}
+	printf("key dosent exist %s\n", ent_cpy);
 	lst = ft_lstnew(ent_cpy);
 	if (!lst)
 		return (free(ent_cpy), E_ALLOCATE);
--- src/5_env/env_get.c
+++ src/5_env/env_get.c
@@ -6,7 +6,7 @@
 /*   By: ymizukam <ymizukam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 19:48:44 by ymizukam          #+#    #+#             */
-/*   Updated: 2024/12/18 19:58:31 by ymizukam         ###   ########.fr       */
+/*   Updated: 2024/12/18 20:49:20 by ymizukam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,11 @@ char	*env_get(t_list *env, char *key)
 {
 	char	uniq_key[MAX_PATH];
 	t_list	*lst;
-	char	*unique_key;
 
 	// printf("%s\n",__func__);
 	ft_strlcpy(uniq_key, key, MAX_PATH);
 	ft_strlcat(uniq_key, "=", MAX_PATH);
 	lst = ft_list_find(env, uniq_key, __cmp);
-	unique_key = malloc(ft_strlen(key) + 2);
-	lst = ft_list_find(env, key, __cmp);
 	// note leak!!!MAX_PATHとして持っても良い
 	if (!lst)
 		return (ft_strdup(""));
