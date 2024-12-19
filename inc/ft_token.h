61b5dd77abd752d13c3489db49fd487ccd7a9fc6
Tetsuro Ando <tetsuro997@gmail.com>
2024-12-20 02:48:39 +0900

    new structs

--- inc/ft_token.h
+++ inc/ft_token.h
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_token.h                                         :+:      :+:    :+:   */
@@ -6,16 +6,16 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 18:20:59 by teando            #+#    #+#             */
-/*   Updated: 2024/12/18 17:51:54 by teando           ###   ########.fr       */
+/*   Updated: 2024/12/20 02:48:10 by teando           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #ifndef TOKEN_H
-# define TOKEN_H
+#define TOKEN_H
 
 typedef enum e_status
 {
-	E_NONE = 0, //正常
+	E_NONE = 0, // 正常
 	E_ALLOCATE = 1,
 	E_ENV_KEY = 1,
 	E_SYNTAX = 1,
@@ -23,44 +23,47 @@ typedef enum e_status
 	E_COMMAND_NOT_FOUND = 127,
 	E_SIGINT = 130,
 	E_SIGQUIT = 131,
-}						t_status;
+} t_status;
 
 typedef enum e_token_type
 {
-	TT_CMD,          // コマンドトークン
-	TT_PIPE,         // パイプ
-	TT_REDIRECT_IN,  // 入力リダイレクト
+	TT_CMD,			 // コマンドトークン
+	TT_PIPE,		 // パイプ
+	TT_REDIRECT_IN,	 // 入力リダイレクト
 	TT_REDIRECT_OUT, // 出力リダイレクト
-	TT_APPEND,       // 追記リダイレクト
-	TT_HEREDOC,      // ヒアドキュメント
-	TT_AND_AND,      // &&
-	TT_OR_OR,        // ||
-	TT_LPAREN,       // (
-	TT_RPAREN,       // )
-	TT_EOF,          // 終端
-	TT_ERROR         // エラー
-}						t_token_type;
-
-typedef struct s_token
-{
-	t_token_type		type;
-	char				*value;
-}						t_token;
-
-typedef struct s_cmd_token
-{
-	t_token_type type; // CMD or PIPEなど
-	char *path;        // 実行するコマンド(e.g. "echo")
-	char **args;       // 引数列(e.g. {"echo", "a", "b", "c", "d", NULL})
-}						t_cmd_token;
+	TT_APPEND,		 // 追記リダイレクト
+	TT_HEREDOC,		 // ヒアドキュメント
+	TT_LPAREN,		 // (
+	TT_RPAREN,		 // )
+	TT_AND_AND,		 // &&
+	TT_OR_OR,		 // ||
+	TT_SEMICOLON,	 // ;
+	TT_EOF,			 // 終端
+	TT_ERROR		 // エラー
+} t_token_type;
 
 typedef struct s_ast_node
 {
-	t_token_type		type;
-	// int                    file_type;
-	char				**args;
-	struct s_ast_node	*left;
-	struct s_ast_node	*right;
-}						t_ast_node;
+	enum
+	{
+		NT_CMD,		 // words and redirects head ptr
+		NT_PIPE,	 // |
+		NT_PRIORITY, // ()
+		NT_EOP,		 // && || ; EOF
+	} e_type;
+	union
+	{
+		t_list *args; // CMDのitems WORDとリダイレクトの先頭ポインタ
+		int pipefds[2];
+		int priority_level;
+		t_token_type eop_type;
+	} u_val;
+} t_ast_node_item;
+
+typedef struct s_token
+{
+	t_token_type type;
+	char *value;
+} t_token;
 
 #endif
